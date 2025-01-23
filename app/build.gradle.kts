f1a65f084dc3f655818f7274981d9bd02f985521
Cristian <cristianalejandro172431@gmail.com>
2025-01-22 21:01:38 -0500

    First Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,7 +36,9 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
--- app/src/main/java/com/example/smartgrow/MainActivity.java
+++ app/src/main/java/com/example/smartgrow/MainActivity.java
@@ -1,37 +1,207 @@
 package com.example.smartgrow;
 
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.smartgrow.databinding.ActivityMainBinding;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        FirebaseApp.initializeApp(MainActivity.this);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        FloatingActionButton add = findViewById(R.id.addplant);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View view1 = LayoutInflater.from(MainActivity.this).inflate(R.layout.add_plant_dialog, null);
+                TextInputLayout titleLayout, contentLayout;
+                titleLayout = view1.findViewById(R.id.titleLayout);
+                contentLayout = view1.findViewById(R.id.contentLayout);
+                TextInputEditText titleET, contentET;
+                titleET = view1.findViewById(R.id.titleET);
+                contentET = view1.findViewById(R.id.contentET);
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Add")
+                        .setView(view1)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if (Objects.requireNonNull(titleET.getText()).toString().isEmpty()) {
+                                    titleLayout.setError("This field is required!");
+                                } else if (Objects.requireNonNull(contentET.getText()).toString().isEmpty()) {
+                                    contentLayout.setError("This field is required!");
+                                } else {
+                                    ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+                                    dialog.setMessage("Storing in Database...");
+                                    dialog.show();
+                                    Plant Plant = new Plant();
+                                    Plant.setName(titleET.getText().toString());
+                                    Plant.setDescription(contentET.getText().toString());
+                                    database.getReference().child("Plants").push().setValue(Plant).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            dialog.dismiss();
+                                            dialogInterface.dismiss();
+                                            Toast.makeText(MainActivity.this, "Saved Successfully!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            dialog.dismiss();
+                                            Toast.makeText(MainActivity.this, "There was an error while saving data", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create();
+                alertDialog.show();
+            }
+        });
+
+        TextView empty = findViewById(R.id.empty);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+
+        database.getReference().child("Plants").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Plant> arrayList = new ArrayList<>();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Plant Plant = dataSnapshot.getValue(Plant.class);
+                    Objects.requireNonNull(Plant).setKey(dataSnapshot.getKey());
+                    arrayList.add(Plant);
+                }
+
+                if (arrayList.isEmpty()) {
+                    empty.setVisibility(View.VISIBLE);
+                    recyclerView.setVisibility(View.GONE);
+                } else {
+                    empty.setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+
+                PlantAdapter adapter = new PlantAdapter(MainActivity.this, arrayList);
+                recyclerView.setAdapter(adapter);
+
+                adapter.setOnItemClickListener(new PlantAdapter.OnItemClickListener() {
+                    @Override
+                    public void onClick(Plant Plant) {
+                        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.add_plant_dialog, null);
+                        TextInputLayout titleLayout, contentLayout;
+                        TextInputEditText titleET, contentET;
+
+                        titleET = view.findViewById(R.id.titleET);
+                        contentET = view.findViewById(R.id.contentET);
+                        titleLayout = view.findViewById(R.id.titleLayout);
+                        contentLayout = view.findViewById(R.id.contentLayout);
+
+                        titleET.setText(Plant.getName());
+                        contentET.setText(Plant.getDescription());
+
+                        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Edit")
+                                .setView(view)
+                                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        if (Objects.requireNonNull(titleET.getText()).toString().isEmpty()) {
+                                            titleLayout.setError("This field is required!");
+                                        } else if (Objects.requireNonNull(contentET.getText()).toString().isEmpty()) {
+                                            contentLayout.setError("This field is required!");
+                                        } else {
+                                            progressDialog.setMessage("Saving...");
+                                            progressDialog.show();
+                                            Plant Plant1 = new Plant();
+                                            Plant1.setName(titleET.getText().toString());
+                                            Plant1.setDescription(contentET.getText().toString());
+                                            database.getReference().child("Plants").child(Plant.getKey()).setValue(Plant1).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+                                                    progressDialog.dismiss();
+                                                    dialogInterface.dismiss();
+                                                    Toast.makeText(MainActivity.this, "Saved Successfully!", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(MainActivity.this, "There was an error while saving data", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                        }
+                                    }
+                                })
+                                .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                })
+                                .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        progressDialog.setTitle("Deleting...");
+                                        progressDialog.show();
+                                        database.getReference().child("Plants").child(Plant.getKey()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                progressDialog.dismiss();
+                                                Toast.makeText(MainActivity.this, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                progressDialog.dismiss();
+                                            }
+                                        });
+                                    }
+                                }).create();
+                        alertDialog.show();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
-
 }
