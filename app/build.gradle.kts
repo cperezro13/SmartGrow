037253f6e2d9095603a6a1c511774f90b0cfbcee
Cristian <cristianalejandro172431@gmail.com>
2025-02-26 17:35:00 -0500

    Added button to test two apis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,18 +40,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
-
     // Importa Firebase BoM (Bill of Materials) para manejar versiones automáticamente
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
-
     // Firebase Authentication
     implementation("com.google.firebase:firebase-auth")
-
     // Firebase Realtime Database
     implementation("com.google.firebase:firebase-database")
-
     implementation("org.tensorflow:tensorflow-lite:2.9.0")
-
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // OkHttp para solicitudes HTTP
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    // Biblioteca org.json para manejar JSON
+    implementation("org.json:json:20210307")
+    // Bibliotecas de Android Jetpack para actividades y fragmentos
+    implementation("androidx.activity:activity-ktx:1.3.1")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    // Glide para cargar imágenes
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0") // Para Kotlin, usa kapt en lugar de annotationProcessor
 }
